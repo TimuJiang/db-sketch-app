6069916a924bd48957812ca0be2015b4198cf25e
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-08-30 18:35:43 +0800

    update

--- app/controller/project.js
+++ app/controller/project.js
@@ -5,16 +5,10 @@ class ProjectController extends Controller {
   async create() {
     const { name, tables, links } = this.ctx.request.body;
     const project = new this.ctx.model.Project({ user_id: this.ctx.session.user.id, name, tables, links });
-    project.save()
-      .then(res => {
-        console.log(res);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    const res = await project.save()
     this.ctx.body = {
       code: 0,
-      data: {},
+      data: res,
       message: 'success',
     };
   }
